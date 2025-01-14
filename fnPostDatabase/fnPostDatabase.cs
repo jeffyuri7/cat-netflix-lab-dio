030a28e751c86d6d1ce1272f191c78f18095ec38
Jefferson Yuri <jeffyuri@hotmail.com>
2025-01-14 15:52:45 -0300

    Inicializa fnPostDatabase

---
+++ fnPostDatabase/fnPostDatabase.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace fnPostDatabase
+{
+    public class fnPostDatabase
+    {
+        private readonly ILogger<fnPostDatabase> _logger;
+
+        public fnPostDatabase(ILogger<fnPostDatabase> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("movie")]
+        [CosmosDBOutput(
+            "%DatabaseName%",
+            "movies",
+            Connection = "CosmosDBConnection",
+            CreateIfNotExists = true,
+            PartitionKey = "id"
+        )]
+        public async Task<object?> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req
+        )
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            MovieRequest movie = null;
+
+            var content = await new StreamReader(req.Body).ReadToEndAsync();
+
+            try
+            {
+                movie = JsonConvert.DeserializeObject<MovieRequest>(content);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult("Erro ao deserializar o objeto: " + ex.Message);
+            }
+
+            return JsonConvert.SerializeObject(movie);
+        }
+    }
+}
